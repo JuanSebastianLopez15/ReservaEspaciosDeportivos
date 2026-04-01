a11b1ce2283c0650b24388aee1c5876a23a0679d
Juan Sebastian Lopez Guzman <juanse150906@gmail.com>
2026-03-31 19:50:01 -0500

    se subieron modificaciones en el proyecto, agregando archivos .env y en modules se agregaron importaciones para usar mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+      // 2. Cargas las variables de entorno globalmente
+      ConfigModule.forRoot({ isGlobal: true }),
+
+      // 3. Configuras la base de datos leyendo el .env
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: 'mysql',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true, // Crea las tablas automáticamente (¡Solo usar en desarrollo!)
+        }),
+      }),
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
